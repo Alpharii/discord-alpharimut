4700253b015b88a004bd72efb81180c54b4c44ca
Alpharii <alphari404@gmail.com>
2024-12-07 10:14:32 +0700

    first commit and setup

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
